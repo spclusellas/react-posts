d92c2313e26ee12f0571b073fee734b97a707e0c
Pio Clusellas <spclusellas@gmail.com>
2020-08-28 18:47:45 -0300

    search show

--- src/components/SearchPost.js
+++ src/components/SearchPost.js
@@ -9,6 +9,7 @@ function SearchForm(props){
 
     const [searchResult, setSearchResults] = useState(false)
     const [visible, setVisible] = useState(3)
+    const [startVisible, setStartVisible] = useState(0)
     const { register, handleSubmit } = useForm();
 
     const onSubmit = data => {
@@ -31,26 +32,34 @@ function SearchForm(props){
     }
 
     function loadMore(){
+        setStartVisible(startVisible + 3)
         setVisible(visible + 3)
     }
 
     function cleanResults(){
         setSearchResults(false)
         setVisible(3)
+        setStartVisible(0)
     }
 
-    function controlResult(){
+    function resetResults(){
+        setStartVisible(0)
+        setVisible(3)
+    }
+
+    function controlResult(post, i){
         return (
             <div className="control-result">
                 <Alert  variant={'dark'}>
-                    <b>{searchResult.length}</b> RESULTS
+                    <b> {searchResult.length}</b> RESULTS
                 </Alert>
                 <Button variant="outline-info" size="md" block onClick={loadMore}>
-                    Show More
+                    Show Others
                 </Button>
                 <Button className='clean-button' variant="outline-danger" size="md" block onClick={cleanResults}>
-                    Clean Search
+                    Clear 
                 </Button>
+                {}
             </div>
         )
     }
@@ -58,14 +67,14 @@ function SearchForm(props){
     return(
         <div className="search-section">
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" name="title" ref={register}></input>
+                <input placeholder="Search Post.." type="text" name="title" ref={register}></input>
                 <button type='submit'>Search</button>
             </form>
             <section className="cards-section search-results">
-                 {searchResult ? searchResult.slice(0, visible).map(showSearchResults) : '' }
+                 {searchResult ? searchResult.slice(startVisible, visible).map(showSearchResults) : '' }
             </section>
             {searchResult[0]? controlResult(): ''}
-            
+            {visible > searchResult.length + 3? resetResults() : ''}
         </div>
     )
 }
